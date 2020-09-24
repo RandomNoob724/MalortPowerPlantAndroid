a8c4fdd3d1734d1a2298bff37b1f2906e16b5179
daax <daax18mk@student.ju.se>
2020-09-24 15:40:38 +0200

    Implemented the GUI for the first use cases/stories

---
+++ app/src/main/java/com/example/malortpowerplant/WorkerHomeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.malortpowerplant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WorkerHomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_worker_home)
+    }
+}
