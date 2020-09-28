5f4ac0f5e2d1b886f6d62ca9349117665f8cc407
daax <daax18mk@student.ju.se>
2020-09-28 16:44:10 +0200

    Implemented simple pop-up warning notification

--- app/src/main/java/com/example/malortpowerplant/WorkerHomeActivity.kt
+++ app/src/main/java/com/example/malortpowerplant/WorkerHomeActivity.kt
@@ -2,10 +2,26 @@ package com.example.malortpowerplant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AlertDialog
+import java.util.Timer
+import kotlin.concurrent.schedule
 
 class WorkerHomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_worker_home)
+        warningNotification()
+    }
+    private fun warningNotification(){
+        Handler().postDelayed(
+            {
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("RADIATION WARNING")
+                builder.setMessage("You need to leave the room in 10 minutes!")
+                builder.setPositiveButton("OK", null)
+                val dialog: AlertDialog = builder.create()
+                dialog.show()
+            }, 5000)
     }
 }
