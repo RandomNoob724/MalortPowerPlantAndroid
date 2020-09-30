de82551c2e311244aab1537d19067ba2bf40c28a
Emil Persson <emilper1999@gmail.com>
2020-09-30 15:22:07 +0200

    Bluetoothhandler for handling bluetooth, main activity handles smaller changes

--- app/src/main/java/com/example/malortpowerplant/BluetoothHandler.kt
+++ app/src/main/java/com/example/malortpowerplant/BluetoothHandler.kt
@@ -1,60 +1,77 @@
 package com.example.malortpowerplant
 
+import android.R
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothSocket
+import android.content.ContentValues.TAG
 import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Message
+import android.provider.Settings.Global.DEVICE_NAME
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import java.io.BufferedInputStream
-import java.io.IOException
+import java.io.*
 import java.util.*
+import kotlinx.coroutines.*
 
-class BluetoothHandler(activity: MainActivity): AppCompatActivity() {
+object BluetoothHandler : Thread(){
+    val bluetoothScope = CoroutineScope(Dispatchers.IO)
     private var bluetoothSocket: BluetoothSocket? = null
-    public fun createBluetoothConnectionWithId(id: String, uuid: UUID) : BluetoothSocket {
+
+    fun createBluetoothConnectionWithId(id: String, uuid: UUID) {
         var bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
         var bluetoothDevice: BluetoothDevice? = null
 
         try {
             if(bluetoothAdapter != null){
-                if(!bluetoothAdapter.isEnabled){
-                    val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-                    startActivityForResult(enableBtIntent, 1)
-                }
-                else if(bluetoothAdapter.isEnabled){
+                if(bluetoothAdapter.isEnabled){
                     bluetoothDevice = bluetoothAdapter.getRemoteDevice(id)
-                    bluetoothSocket = bluetoothDevice.createInsecureRfcommSocketToServiceRecord(uuid)
-                    bluetoothSocket!!.connect()
-                    Log.d("bluetooth", "Bluetooth is now connected " + bluetoothSocket!!.isConnected.toString())
+                    this.bluetoothSocket = bluetoothDevice.createInsecureRfcommSocketToServiceRecord(uuid)
+                    if(bluetoothSocket != null){
+                        bluetoothSocket?.connect()
+                    }
+                    Log.d("bluetooth", "Bluetooth is now connected " + bluetoothSocket?.isConnected.toString())
                 }
             }
         } catch(e: IOException){
             Log.d("bluetooth", e.toString())
         }
-        return bluetoothSocket!!
     }
 
-    fun awaitIncomingBluetoothData(bluetoothSocket: BluetoothSocket){
-        var bufferedInputStream: BufferedInputStream?
+    suspend fun awaitIncomingBluetoothData(){
+        var inputStream: DataInputStream?
+        var bufferedOutputStream: BufferedOutputStream?
         var rfid: String = ""
-        bufferedInputStream = BufferedInputStream(bluetoothSocket?.inputStream, 16)
-        for (value in bufferedInputStream){
-            if(rfid.length >= 8){
-                rfid = ""
-            }
-            rfid += value.toChar()
-            Log.d("bluetooth", rfid)
+        val bytes: Int
+        Log.d("bluetooth", (this.bluetoothSocket != null).toString())
+        if(this.bluetoothSocket != null){
+            inputStream = DataInputStream(this.bluetoothSocket?.inputStream)
+            bytes = inputStream.read()
+            Log.d("bluetooth",  bytes.toString())
+        } else {
+            Log.d("bluetooth", "The bluetoothsocket is now null")
         }
     }
 
-    fun getBluetoothSocket(): BluetoothSocket{
-        return bluetoothSocket!!
+    fun checkBluetoothAdapter(): Boolean{
+        var bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+        return bluetoothAdapter?.isEnabled ?: false
+    }
+
+    fun getBluetoothSocket(): BluetoothSocket? {
+        if(this.bluetoothSocket == null){
+            return null
+        } else {
+            return bluetoothSocket as BluetoothSocket
+        }
     }
 
     fun getConnection(): Boolean {
-        if(bluetoothSocket != null){
-            return bluetoothSocket!!.isConnected
+        if(this.bluetoothSocket != null){
+            return this.bluetoothSocket!!.isConnected
         } else {
             return false
         }
--- app/src/main/java/com/example/malortpowerplant/MainActivity.kt
+++ app/src/main/java/com/example/malortpowerplant/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.malortpowerplant
 
 import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothSocket
 import android.content.Intent
 import android.os.Bundle
@@ -9,44 +8,46 @@ import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentActivity
-import java.io.BufferedInputStream
-import java.io.DataInputStream
-import java.io.IOException
-import java.io.InputStream
-import java.lang.reflect.Method
 import java.util.*
-import com.example.malortpowerplant.BluetoothHandler
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val bluetoothHandler: BluetoothHandler = BluetoothHandler(this)
         val uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb")
         val bluetoothChipID = "98:D3:11:F8:6B:81"
         var bluetoothSocket: BluetoothSocket? = null
         var connectButton = findViewById<Button>(R.id.connectButton)
         var text = findViewById<TextView>(R.id.fucking_text)
 
+        if(!BluetoothHandler.checkBluetoothAdapter()){
+            val intent: Intent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(intent,1)
+        }
+
         connectButton.setOnClickListener{
-            Log.d("bluetooth", bluetoothHandler.getConnection().toString())
-            if(bluetoothHandler.getConnection()){
+            Log.d("bluetooth", BluetoothHandler.getConnection().toString())
+            if(BluetoothHandler.getConnection()){
                 Log.d("bluetooth", "Disconnecting Socket")
-                bluetoothSocket!!.close()
+                BluetoothHandler.getBluetoothSocket()!!.close()
+                BluetoothHandler.bluetoothScope.cancel()
                 fucking_text.text = "Disconnected"
             } else {
                 Log.d("bluetooth", "Connecting Socket")
-                bluetoothSocket = bluetoothHandler.createBluetoothConnectionWithId(bluetoothChipID, uuid)
+                BluetoothHandler.createBluetoothConnectionWithId(bluetoothChipID, uuid)
                 fucking_text.text = "Connected"
             }
+            if(BluetoothHandler.getConnection()){
+                fucking_text.text = "Connected"
+                BluetoothHandler.bluetoothScope.launch {
+                    BluetoothHandler.awaitIncomingBluetoothData()
+                }
+            } else {
+                fucking_text.text = "Error"
+            }
         }
-
-
-
-        //bluetoothHandler.awaitIncomingBluetoothData(bluetoothSocket)
-
     }
 }
