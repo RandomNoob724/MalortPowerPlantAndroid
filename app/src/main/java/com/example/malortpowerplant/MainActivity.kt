276564eea5c3c9055bab965743094793957a5feb
daax <daax18mk@student.ju.se>
2020-09-30 16:36:47 +0200

    Updated and linked the GUI with Bluetooth functionality

--- app/src/main/java/com/example/malortpowerplant/MainActivity.kt
+++ app/src/main/java/com/example/malortpowerplant/MainActivity.kt
@@ -5,26 +5,31 @@ import android.bluetooth.BluetoothSocket
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_start.*
 import kotlinx.coroutines.*
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_start)
 
-        CloudFirestore.instance.updateClockedIn("36 1F 1F F8")
+        CloudFirestore.instance.updateClockedIn("361F1FF8")
 
         val uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb")
         val bluetoothChipID = "98:D3:11:F8:6B:81"
         var bluetoothSocket: BluetoothSocket? = null
         var connectButton = findViewById<Button>(R.id.connectButton)
-        var text = findViewById<TextView>(R.id.fucking_text)
+        var text = findViewById<TextView>(R.id.connectionLabel)
+        var loadingIndicator = findViewById<ProgressBar>(R.id.loadingIndicator)
+
+        loadingIndicator.visibility = View.VISIBLE
 
         if(!BluetoothHandler.checkBluetoothAdapter()){
             val intent: Intent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
@@ -37,19 +42,26 @@ class MainActivity : AppCompatActivity() {
                 Log.d("bluetooth", "Disconnecting Socket")
                 BluetoothHandler.getBluetoothSocket()!!.close()
                 BluetoothHandler.bluetoothScope.cancel()
-                fucking_text.text = "Disconnected"
+                connectionLabel.text = "Disconnected"
+                loadingIndicator.visibility = View.GONE
             } else {
                 Log.d("bluetooth", "Connecting Socket")
                 BluetoothHandler.createBluetoothConnectionWithId(bluetoothChipID, uuid)
-                fucking_text.text = "Connected"
+                connectionLabel.text = "Connected"
+                loadingIndicator.visibility = View.GONE
             }
             if(BluetoothHandler.getConnection()){
-                fucking_text.text = "Connected"
+                connectionLabel.text = "Connected"
+                loadingIndicator.visibility = View.GONE
                 BluetoothHandler.bluetoothScope.launch {
                     BluetoothHandler.awaitIncomingBluetoothData()
                 }
+                val intent = Intent(this, WorkerHomeActivity::class.java)
+                startActivity(intent)
+                finish()
             } else {
-                fucking_text.text = "Error"
+                connectionLabel.text = "Error"
+                loadingIndicator.visibility = View.GONE
             }
         }
     }
